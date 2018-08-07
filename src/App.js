1409ab663457091cf38901220401c77e50f7261b
parkhanseong <hs88610@gmail.com>
2018-08-07 13:32:15 +0900

    modify app.js

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
         onCreate_={this.handleCreate}
         />
         <PhoneInfoList data={this.state.information}/>
+        <PhoneInfo/>
       </div>
     );
   }
